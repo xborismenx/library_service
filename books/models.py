9f98ec3aa9ecfb2bede246a56f88c76690d54af9
xborismenx <boris.tikhonov.21@gmail.com>
2024-11-16 13:49:25 +0200

    initialized Books model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Books(models.Model):
+    CHOICES = {
+        "HARD": "HARD",
+        "SOFT": "SOFT",
+    }
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    cover = models.CharField(CHOICES, max_length=100)
+    inventory = models.PositiveIntegerField()
+    Daily_fee = models.DecimalField(max_digits=5, decimal_places=2)
