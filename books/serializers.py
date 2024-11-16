30de7aecaec1e835584faca6a7feb3c7f637172c
xborismenx <boris.tikhonov.21@gmail.com>
2024-11-16 14:37:32 +0200

    added logic to BookSerializer

--- books/serializers.py
+++ books/serializers.py
@@ -1,5 +1,9 @@
 from rest_framework import serializers
 
+from books.models import Books
+
 
 class BookSerializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model = Books
+        id = ("title", "author", "cover", "inventory", "Daily_fee")
