0b9626b169114477a9a6b89d3b26845760814d51
xborismenx <boris.tikhonov.21@gmail.com>
2024-11-16 21:14:03 +0200

    changed "api/book/" to empty path

--- library_service/urls.py
+++ library_service/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/book/", include("books.urls")),
+    path("", include("books.urls")),
 ]
