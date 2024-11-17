ae2e0e8ce20b702209aa793948b228443b89e004
xborismenx <boris.tikhonov.21@gmail.com>
2024-11-17 22:24:04 +0200

    added permissions into BookViewSet

--- books/views.py
+++ books/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny, IsAdminUser
 
 from books.models import Books
 from books.serializers import BookSerializer
@@ -7,3 +8,8 @@ from books.serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Books.objects.all()
     serializer_class = BookSerializer
+
+    def get_permissions(self):
+        if self.action == 'list':
+            self.permission_classes = [AllowAny(), ]
+        return [IsAdminUser(), ]
