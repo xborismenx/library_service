3f7760e36bc87f07d8dbce116ab5c82813ad173a
xborismenx <boris.tikhonov.21@gmail.com>
2024-11-23 17:36:06 +0200

    initialized celery, celery_beat settings & installed redis

---
+++ library_service/celery.py
@@ -0,0 +1,13 @@
+import os
+from celery import Celery
+from django.conf import settings
+
+# Set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'library_service.settings')
+
+app = Celery('library_service')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load tasks from all registered Django app configs.
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
