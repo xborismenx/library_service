7b65711629c5fdeece0c32c9d208d9126cd4e21a
xborismenx <boris.tikhonov.21@gmail.com>
2024-11-30 01:52:44 +0200

    switched task execution to Django Q from Celery

--- borrowings/tasks.py
+++ borrowings/tasks.py
@@ -1,25 +1,21 @@
 import asyncio
 from os import getenv
 
-from aiogram import Bot
-from celery import shared_task
-
 from dotenv import load_dotenv
+from notifications.bot import bot
+
+from notifications.borrowers import borrowers_overdue
 
 load_dotenv()
 
 TOKEN = getenv("BOT_API_TOKEN")
-bot = Bot(token=TOKEN)
+chat_id = getenv("CHAT_ID")
 
-@shared_task
+
 def overdue_borrowings():
-
-    message = "Ваше сообщение: ---"
-    chat_id = 393532316
-
+    message = borrowers_overdue()
     asyncio.run(send_message(message, chat_id))
 
 
-async def send_message(message, chat_id):
-        await bot.send_message(chat_id=chat_id, text=message)
-
+async def send_message(message: str, chat: int) -> None:
+    await bot.send_message(chat_id=chat_id, text=message)
