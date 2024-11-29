519f8926f2bbbfbf1518883f49b26d5416f1956f
xborismenx <boris.tikhonov.21@gmail.com>
2024-11-30 01:44:34 +0200

    initialized __str__ method

--- borrowings/models.py
+++ borrowings/models.py
@@ -12,7 +12,8 @@ class Borrowing(models.Model):
     actual_return_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
-        return f"{self.user.first_name} {self.user.last_name} borrowed {self.book.title}"
+        return (f"{self.user.first_name} {self.user.last_name} "
+                f"(email:{self.user.email}) borrowed {self.book}")
 
     class Meta:
         unique_together = ('user', 'book')
