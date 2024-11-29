e7ef5c18cb143ef48d71ac73ab6fa6b1368ba2b5
xborismenx <boris.tikhonov.21@gmail.com>
2024-11-30 01:39:27 +0200

    delete handlers from main()

--- notifications/bot.py
+++ notifications/bot.py
@@ -5,7 +5,6 @@ from os import getenv
 
 from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
-from handlers import register_handlers
 
 load_dotenv()
 
@@ -14,9 +13,7 @@ TOKEN = getenv("BOT_API_TOKEN")
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
-
 async def main() -> None:
-    register_handlers(dp, bot)
     await dp.start_polling(bot)
 
 
